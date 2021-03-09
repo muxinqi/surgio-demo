97aed35b454da811fe5fcca681aa86d4f21fb725
muxinqi <hi@mxq.im>
2021-03-09 18:13:12 +0800

    init: npm init surgio-store surgio-demo

---
+++ surgio.conf.js
@@ -0,0 +1,168 @@
+'use strict';
+
+const { utils } = require('surgio');
+
+/**
+ * 使用文档：https://surgio.royli.dev/
+ */
+module.exports = {
+  /**
+   * 远程片段
+   * 文档：https://surgio.royli.dev/guide/custom-config.html#remotesnippets
+   */
+  remoteSnippets: [
+    {
+      name: 'apple', // 模板中对应 remoteSnippets.apple
+      url: 'https://raw.githubusercontent.com/geekdada/surge-list/master/surgio-snippet/apple.tpl',
+      surgioSnippet: true
+    },
+    {
+      name: 'telegram', // 模板中对应 remoteSnippets.telegram
+      url: 'https://raw.githubusercontent.com/DivineEngine/Profiles/master/Surge/Ruleset/Extra/Telegram/Telegram.list'
+    },
+    {
+      name: 'netflix', // 模板中对应 remoteSnippets.netflix
+      url: 'https://github.com/lhie1/Rules/raw/master/Surge/Surge%203/Provider/Media/Netflix.list'
+    },
+    {
+      name: 'hbo', // 模板中对应 remoteSnippets.hbo
+      url: 'https://github.com/lhie1/Rules/raw/master/Surge/Surge%203/Provider/Media/HBO.list'
+    },
+    {
+      name: 'hulu', // 模板中对应 remoteSnippets.hulu
+      url: 'https://raw.githubusercontent.com/geekdada/surge-list/master/hulu.list'
+    },
+    {
+      name: 'disney', // 模板中对应 remoteSnippets.disney
+      url: 'https://raw.githubusercontent.com/geekdada/surge-list/master/disney.list',
+    },
+    {
+      name: 'paypal', // 模板中对应 remoteSnippets.paypal
+      url: 'https://github.com/DivineEngine/Profiles/raw/master/Surge/Ruleset/Extra/PayPal.list'
+    },
+  ],
+  customFilters: {
+    hktFilter: utils.useKeywords(['hkt', 'HKT']),
+  },
+  artifacts: [
+    /**
+     * Surge
+     */
+    {
+      name: 'SurgeV3.conf', // 新版 Surge
+      template: 'surge_v3',
+      provider: 'demo',
+    },
+    {
+      name: 'Surge_simple.conf', // 旧版 Surge
+      template: 'surge_simple',
+      provider: 'subscribe_demo',
+    },
+    // 合并 Provider
+    {
+      name: 'SurgeV3_combine.conf',
+      template: 'surge_v3',
+      provider: 'demo',
+      combineProviders: ['subscribe_demo'],
+    },
+    // Surge + SSR
+    {
+      name: 'SurgeV3_ssr.conf',
+      template: 'surge_v3',
+      provider: 'ssr_subscribe_demo',
+    },
+
+    /**
+     * Clash
+     */
+    {
+      name: 'Clash.yaml',
+      template: 'clash',
+      provider: 'subscribe_demo',
+    },
+    {
+      name: 'Clash_custom_dns.yaml',
+      template: 'clash',
+      provider: 'subscribe_demo',
+      customParams: {
+        dns: true,
+      }
+    },
+
+    /**
+     * Quantumult
+     */
+    {
+      name: 'Quantumult_rules.conf',
+      template: 'quantumult_rules',
+      provider: 'subscribe_demo',
+    },
+    {
+      name: 'Quantumult.conf',
+      template: 'quantumult',
+      provider: 'subscribe_demo',
+    },
+    {
+      // 给 Quantumult 提供订阅地址
+      name: 'Shadowsocks_subscribe.conf',
+      template: 'shadowsocks_subscribe',
+      provider: 'subscribe_demo',
+    },
+
+    /**
+     * Quantumult X
+     */
+    {
+      name: 'QuantumultX_rules.conf',
+      template: 'quantumultx_rules',
+      provider: 'demo',
+    },
+    {
+      name: 'QuantumultX.conf',
+      template: 'quantumultx',
+      provider: 'demo',
+    },
+    {
+      name: 'QuantumultX_subscribe_us.conf',
+      template: 'quantumultx_subscribe',
+      provider: 'demo',
+      customParams: {
+        magicVariable: utils.usFilter,
+      },
+    },
+    {
+      name: 'QuantumultX_subscribe_hk.conf',
+      template: 'quantumultx_subscribe',
+      provider: 'demo',
+      customParams: {
+        magicVariable: utils.hkFilter,
+      },
+    },
+  ],
+  /**
+   * 订阅地址的前缀部分，以 / 结尾
+   * 例如阿里云 OSS 的访问地址 https://xxx.oss-cn-hangzhou.aliyuncs.com/
+   */
+  urlBase: 'https://example.com/',
+  surgeConfig: {
+    v2ray: 'native', // 默认 'native', 可选 'external'
+    shadowsocksFormat: 'ss', // 默认 'ss', 可选 'custom'
+  },
+  binPath: {
+    // 安装教程: https://surgio.royli.dev/guide/install-ssr-local.html
+    shadowsocksr: '/usr/local/bin/ssr-local',
+  },
+  upload: {
+    // 默认保存至根目录，可以在此修改子目录名，以 / 结尾，默认为 /
+    prefix: '/',
+    bucket: 'surgio-store',
+    // 支持所有区域
+    region: 'oss-cn-hangzhou',
+    // 以下信息于阿里云控制台获得
+    accessKeyId: 'YOUR_ACCESS_KEY_ID',
+    accessKeySecret: 'YOUR_ACCESS_KEY_SECRET',
+  },
+  // 非常有限的报错信息收集
+  analytics: true,
+};
+
---
+++ template/surge_v3.tpl
@@ -0,0 +1,86 @@
+#!MANAGED-CONFIG {{ downloadUrl }} interval=43200 strict=false
+
+{% import './snippet/direct_rules.tpl' as direct_rules %}
+{% import './snippet/youtube_rules.tpl' as youtube_rules %}
+{% import './snippet/us_rules.tpl' as us_rules %}
+{% import './snippet/blocked_rules.tpl' as blocked_rules %}
+
+[General]
+# 日志等级: warning, notify, info, verbose (默认值: notify)
+loglevel = notify
+# 跳过某个域名或者 IP 段，这些目标主机将不会由 Surge Proxy 处理。(在 macOS
+# 版本中，如果启用了 Set as System Proxy,  那么这些值会被写入到系统网络代理
+# 设置中.)
+skip-proxy = 127.0.0.1, 192.168.0.0/16, 10.0.0.0/8, 172.16.0.0/12, 100.64.0.0/10, 100.84.0.0/10, localhost, *.local
+# 强制使用特定的 DNS 服务器
+dns-server = system, 119.29.29.29, 223.5.5.5, 1.1.1.1
+
+# 将特定 IP 段跳过 Surge TUN，详见 Manual
+bypass-tun = 192.168.0.0/16, 10.0.0.0/8, 172.16.0.0/12
+# 是否截取并保存 HTTP 流量 (启用后将对性能有较大影响) (默认值: false)
+replica = false
+# 是否启动完整的 IPv6 支持 (默认值: false)
+ipv6 = false
+
+# 以下参数仅供 macOS 版本使用（多端口监听仅 Surge 3 支持）
+http-listen = 0.0.0.0:6152
+socks5-listen = 0.0.0.0:6153
+
+# 测速地址
+internet-test-url = {{ proxyTestUrl }}
+proxy-test-url = {{ proxyTestUrl }}
+
+# 其它
+# external-controller-access = password@0.0.0.0:6170
+show-primary-interface-changed-notification = true
+proxy-settings-interface = Primary Interface (Auto)
+menu-bar-show-speed = false
+allow-wifi-access = true
+hide-crashlytics-request = true
+
+[Proxy]
+{{ getSurgeNodes(nodeList) }}
+
+[Proxy Group]
+🚀 Proxy = select, {{ getNodeNames(nodeList) }}
+🎬 Netflix = select, {{ getNodeNames(nodeList, netflixFilter) }}
+📺 YouTube = select, 🚀 Proxy, US, HK
+🍎 Apple = select, DIRECT, 🚀 Proxy, US, HK
+🍎 Apple CDN = select, DIRECT, 🍎 Apple
+US = url-test, {{ getNodeNames(nodeList, usFilter) }}, url = {{ proxyTestUrl }}, interval = 1200
+HK = url-test, {{ getNodeNames(nodeList, hkFilter) }}, url = {{ proxyTestUrl }}, interval = 1200
+
+[Rule]
+{{ remoteSnippets.apple.main('🚀 Proxy', '🍎 Apple', '🍎 Apple CDN', 'DIRECT', 'US') }}
+
+{{ remoteSnippets.netflix.main('🎬 Netflix') }}
+
+{{ remoteSnippets.hbo.main('🎬 Netflix') }}
+
+{{ remoteSnippets.hulu.main('🚀 Proxy') }}
+
+{{ youtube_rules.main('📺 YouTube') }}
+
+{{ us_rules.main('US') }}
+
+{{ remoteSnippets.telegram.main('🚀 Proxy') }}
+
+{{ blocked_rules.main('🚀 Proxy') }}
+
+{{ direct_rules.main('DIRECT') }}
+
+# Rulesets
+RULE-SET,SYSTEM,DIRECT
+
+# LAN
+RULE-SET,LAN,DIRECT
+
+# GeoIP CN
+GEOIP,CN,DIRECT
+
+# Final
+FINAL,🚀 Proxy,dns-failed
+
+[URL Rewrite]
+^https?://(www.)?g.cn https://www.google.com 302
+^https?://(www.)?google.cn https://www.google.com 302
